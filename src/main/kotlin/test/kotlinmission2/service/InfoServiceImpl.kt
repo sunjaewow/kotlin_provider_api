2824c759fba821dfe3e55fe4be7105c9be6681e1
sunjaewow <chltjswo789@naver.com>
2025-08-12 20:44:39 +0900

    fix: providerFactory 제거 후 구현체에 @Order()추가

--- src/main/kotlin/test/kotlinmission2/service/InfoServiceImpl.kt
+++ src/main/kotlin/test/kotlinmission2/service/InfoServiceImpl.kt
@@ -2,14 +2,14 @@ package test.kotlinmission2.service
 
 import org.springframework.stereotype.Service
 import test.kotlinmission2.dto.Info
-import test.kotlinmission2.factory.ProviderFactory
+import test.kotlinmission2.provider.Provider
 import java.lang.RuntimeException
 
 @Service
-class InfoServiceImpl(private val providerFactory: ProviderFactory): InfoService{
+class InfoServiceImpl(private val providers: List<Provider>): InfoService{
     override fun getInfo(): Info {
         var last : RuntimeException? = null
-        for (provider in providerFactory.getProviders()) {
+        for (provider in providers) {
             try {
                 return provider.getInfo()
             } catch (e: RuntimeException) { last=e;
